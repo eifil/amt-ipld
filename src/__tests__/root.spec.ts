607639b3a7bd495ec992a6ec9225d474006b604d
Alan Shaw <alan.shaw@protocol.ai>
2021-01-28 11:44:36 +0000

    test: moar delete tests

--- src/__tests__/root.spec.ts
+++ src/__tests__/root.spec.ts
@@ -243,12 +243,95 @@ test('insert a bunch with delete', async t => {
   c = await na.flush()
   const n2a = await AMT.load(bs, c)
 
-  console.log(`originSN: ${originSet.size}, removeSN: ${removeSet.size}; expected: ${originSet.size - removeSet.size}, actual len(n2a): ${n2a.size}`)
-  assertSize(t, n2a, BigInt(originSet.size - removeSet.size))
+  t.is(
+    n2a.size,
+    BigInt(originSet.size - removeSet.size),
+    `originSN: ${originSet.size}, removeSN: ${removeSet.size}; expected: ${originSet.size - removeSet.size}, actual len(n2a): ${n2a.size}`
+  )
 
   for (let i = 0; i < num; i++) {
     if (originSet.has(i) && !removeSet.has(i)) {
       await assertGet(t, n2a, BigInt(i), 'foo foo bar')
     }
   }
+})
+
+test('delete first entry', async t => {
+  const bs = memstore()
+  const a = new AMT<string>(bs)
+
+  await assertSet(t, a, 0n, 'cat')
+  await assertSet(t, a, 27n, 'cat')
+
+  await assertDelete(t, a, 27n)
+
+  const c = await a.flush()
+  const na = await AMT.load(bs, c)
+
+  assertSize(t, na, 1n)
+})
+
+test('delete', async t => {
+  const bs = memstore()
+  const a = new AMT<string>(bs)
+
+  // Check that deleting out of range of the current AMT returns not found
+  const found = await a.delete(200n)
+  t.false(found)
+
+  await assertSet(t, a, 0n, 'cat')
+  await assertSet(t, a, 1n, 'cat')
+  await assertSet(t, a, 2n, 'cat')
+  await assertSet(t, a, 3n, 'cat')
+
+  await assertDelete(t, a, 1n)
+
+  await assertGet(t, a, 0n, 'cat')
+  await assertGet(t, a, 2n, 'cat')
+  await assertGet(t, a, 3n, 'cat')
+
+  await assertDelete(t, a, 0n)
+  await assertDelete(t, a, 2n)
+  await assertDelete(t, a, 3n)
+
+  assertSize(t, a, 0n)
+
+  await assertSet(t, a, 23n, 'dog')
+  await assertSet(t, a, 24n, 'dog')
+
+  await assertDelete(t, a, 23n)
+
+  assertSize(t, a, 1n)
+
+  const c = await a.flush()
+  const na = await AMT.load(bs, c)
+
+  assertSize(t, na, 1n)
+
+  const a2 = new AMT(bs)
+  await assertSet(t, a2, 24n, 'dog')
+
+  const a2c = await a2.flush()
+
+  t.true(c.equals(a2c))
+})
+
+test('delete reduce height', async t => {
+  const bs = memstore()
+  const a = new AMT<string>(bs)
+
+  await assertSet(t, a, 1n, 'thing')
+
+  const c1 = await a.flush()
+
+  await assertSet(t, a, 37n, 'other')
+
+  const c2 = await a.flush()
+  const a2 = await AMT.load(bs, c2)
+
+  await assertDelete(t, a2, 37n)
+  assertSize(t, a2, 1n)
+
+  const c3 = await a2.flush()
+  t.true(c1.equals(c3), 'structures did not match after insert/delete')
 })
